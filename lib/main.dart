1d087447ab911fa8f4dd6f8a2c06f1482e6a3179
PawelS96 <pawelsobieszczuk@wp.pl>
2022-06-23 22:09:48 +0200

    update: change nav bar color on Android 12

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
+import 'dart:io';
+
 import 'package:battlestats/app/app.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
       systemStatusBarContrastEnforced: true,
-      systemNavigationBarColor: Colors.transparent,
+      systemNavigationBarColor: await _getNavBarColor(),
       systemNavigationBarDividerColor: Colors.transparent,
       systemNavigationBarIconBrightness: Brightness.dark,
-      statusBarIconBrightness: Brightness.dark));
-
-  WidgetsFlutterBinding.ensureInitialized();
+      statusBarIconBrightness: Brightness.dark,
+    ),
+  );
 
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge, overlays: [SystemUiOverlay.top]);
 
   runApp(const BattleStatsApp());
+}
+
+Future<Color> _getNavBarColor() async {
+  var color = Colors.transparent;
+  if (Platform.isAndroid) {
+    final info = await DeviceInfoPlugin().androidInfo;
+    final version = info.version.sdkInt ?? 0;
+    if (version >= 31) {
+      return Colors.black;
+    }
+  }
+
+  return color;
 }
